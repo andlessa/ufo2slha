c491552482bf7a1271bf84cd6c571019da6ea835
juhidutta <dutta.juhi91@gmail.com>
2018-06-15 13:42:32 +0530

    Update python script

--- scripts/python_script_mg5.py
+++ scripts/python_script_mg5.py
@@ -1,13 +1,20 @@
 import os
-os.chdir("/home/recapp/Downloads/MG5_aMC_v2_6_2")
-os.system('/home/recapp/Downloads/MG5_aMC_v2_6_2/editparam.py')
-os.chdir("/home/recapp/Downloads/MG5_aMC_v2_6_2/")
-os.system('./bin/mg5_aMC ./text_createfolders.py') 
-os.chdir("/home/recapp/Downloads/MG5_aMC_v2_6_2/")
-os.system('./bin/mg5_aMC ./MG5run.py') 
-os.chdir("/home/recapp/Downloads/MG5_aMC_v2_6_2")
-os.system('/home/recapp/Downloads/MG5_aMC_v2_6_2/cross_section3.py')
-     
-    
+os.chdir("/home/recapp/Downloads/MG5_aMC_v2_6_2/scripts_2")
+os.system('./mg5_aMC ./text_createfolders.py') 
+# edit param card  #default f    too many text files, easier way to input using function
+
+for f in range(1000,1050,50):
+    for k in range(1,2,1):
+        A=open('f.txt','w+')
+        A.write(str(f))
+        B = open('k.txt','w+')
+        B.write(str(k*0.5))
+        #write a function which writes param_card with f, k as inputs 
+      
+        os.system('/home/recapp/Downloads/MG5_aMC_v2_6_2/scripts_2/editparam.py')
+        os.system('./mg5_aMC ./MG5run.py') 
+        #os.chdir("/home/recapp/Downloads/MG5_aMC_v2_6_2/scripts_2/")
+        os.system('/home/recapp/Downloads/MG5_aMC_v2_6_2/scripts_2/cross_section3.py')
+  #  os.system('rm -rf *sample1/Events/*')
 print  "Goodbye"
 
