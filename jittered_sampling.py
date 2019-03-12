67c5a4218ef61255ff0a9b2a09deffaba4937290
Andre Lessa <lessa.a.p@gmail.com>
2019-03-12 13:56:56 -0300

    Small improvements in sampling

--- jittered_sampling.py
+++ jittered_sampling.py
@@ -17,22 +17,37 @@ class ParameterSpace(object):
     and the methods for generating samples of points in the parameter space.
     """
     
-    def __init__(self,parametersDict,n_pieces,maxPoints=None):
+    def __init__(self,parametersDict):
         """
         :param parametersDict: A dictionary with the label for the parameters as keys and a list with the parameter lower and
                                upper limits as values.
-        :param n_pieces: Number of pieces to split each parameter axis. It can be a single integer (all parameters are split
-                         with the same number) or a dictionary with the parameter labels as keys and the number of
-                         partitions as values.
-        :param maxPoints: Maximum allowed number of total points to be generated. If it is not consistent with n_pieces,
-                          the number of partitions will be evenly redefined for each dimension, so the total number of points
-                          matches maxPoints.
         """
         
         self.parameters = np.array(sorted(list(parametersDict.keys())))
         self.minParameters = dict([[par,min(parametersDict[par])] for par in self.parameters])
         self.maxParameters = dict([[par,max(parametersDict[par])] for par in self.parameters])
         self.dim = self.parameters.size
+
+    def get_jittered_samplig(self,n_pieces,maxPoints=None,outFile=None,minJitter=0.,maxJitter=1.):
+        '''
+        Perform jittered sampling
+        :param n_pieces: Number of pieces to split each parameter axis. It can be a single integer (all parameters are split
+                         with the same number) or a dictionary with the parameter labels as keys and the number of
+                         partitions as values.
+        :param maxPoints: Maximum allowed number of total points to be generated. If it is not consistent with n_pieces,
+                          the number of partitions will be evenly redefined for each dimension, so the total number of points
+                          matches maxPoints.
+        :param outFile: Name of outputfile (optional). If given the points will be saved in csv format
+        :param minJitter: Controls the lower limit for jittering the points in each grid box 
+                          (e.g. minJitter=0 allow for points to be jittered from the beginning of the box, while minJitter = 0.1
+                            allow for points to be jittered from the first 10% of the box onwards)
+        :param maxJitter: Controls the upper limit for jittering the points in each grid box 
+                          (e.g. maxJitter=1 allow for points to be jittered till the end of the box, while maxJitter = 0.9
+                            allow for points to be jittered till 90% of the box size)
+
+        '''
+        
+        
         if isinstance(n_pieces,(int,float)):
             self.n_pieces = dict([key,int(n_pieces)] for key in self.parameters)
         elif isinstance(n_pieces,dict):
@@ -57,11 +72,6 @@ class ParameterSpace(object):
         #Store steps in each dimension:
         self.steps = dict([[par,(self.maxParameters[par]-self.minParameters[par])/self.n_pieces[par]] 
                            for par in self.parameters])
-
-    def get_jittered_samplig(self,outFile=None):
-        '''
-        Perform jittered sampling
-        '''
         
         values = {}
         for par in self.parameters:
@@ -77,7 +87,7 @@ class ParameterSpace(object):
         for ipt,pt in enumerate(points):
             pt = list(pt)
             for ipar,par in enumerate(self.parameters):
-                pt[ipar] += np.random.uniform(0.,self.steps[par])                
+                pt[ipar] += np.random.uniform(minJitter*self.steps[par],maxJitter*self.steps[par])
             points[ipt] = tuple(pt)
         
         #Create structured numpy array with all numbers as floats:
@@ -90,10 +100,14 @@ class ParameterSpace(object):
         return points
                 
  
-    def get_random_sampling(self,outFile=None):
-        '''Perform random sampling'''
+    def get_random_sampling(self,Npoints,outFile=None):
+        '''
+        Perform random sampling
         
-        Npoints = np.prod(np.array(list(self.n_pieces.values())))
+        :param Npoints: Total number of points
+        :param outFile: Name of outputfile (optional). If given the points will be saved in csv format
+        '''
+        
         points = []
         minVals = np.array([self.minParameters[par] for par in self.parameters])
         maxVals = np.array([self.maxParameters[par] for par in self.parameters])
